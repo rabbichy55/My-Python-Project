053f1168e964430f7e34f7e9a581717babeadb63
MH Rabbi রাব্বি <rabbichy55@gmail.com>
2026-03-10 03:02:23 +0600

    Clean up repeated message in Auto Message Sending Tool
    Removed redundant message repetitions in the script.

--- Auto Message Sending Tool.py
+++ Auto Message Sending Tool.py
@@ -11,5 +11,3 @@ while message > 0: # This will run the loop until the message variable is greate
     time.sleep(1) # This will wait for 1 second before pressing the keys again.
     pyautogui.press("enter") # This will press the enter key to submit the message.
     message = message - 1 # This will decrease the value of the message variable by 1 after each iteration of the loop.
-I miss you so much.I miss you so much.
-I miss you so much.
